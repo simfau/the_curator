ab0351885d493a8680ad15aa4f030219335595a0
Simon Faucher <simfaucher@outlook.com>
2025-12-09 13:29:33 -0500

    fix on safari for older iphones

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module TheCurator
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.active_record.schema_format = :sql
+    config.hosts << "fb8ffa390ba9.ngrok-free.app"
   end
 end
