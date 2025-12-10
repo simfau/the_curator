761279785029610ccb284ffffde2ba1bbbd77240
Simon Faucher <simfaucher@outlook.com>
2025-12-10 10:10:29 -0500

    avoiding conflits

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,12 +15,12 @@ class PagesController < ApplicationController
   end
   def recommendation
     params[:action_type] == 'movie' ? format = 1 : format = 0
-    contents = params[:content_ids].flatten.map {|content_id| Content.find(content_id)}
-    contents.each do |content|
+    @contents = params[:content_ids].flatten.map {|content_id| Content.find(content_id)}
+    @contents.each do |content|
       ContentTag.new.tagging(content)
     end
     @content = Content.contents_score(params[:content_ids].flatten, format).first
-    @explanation = JSON.parse(explanation(contents, @content).body)['choices'][0]['message']['content']
+    @explanation = JSON.parse(explanation(@contents, @content).body)['choices'][0]['message']['content']
     # @content = @contents.select{ |content| content[:content][:format] == params[:action_type] }.first
   end
 
