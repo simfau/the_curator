495f2631a482c203ee159b2837b857a425853055
Ian Jarvis <imwjarvis@gmail.com>
2025-12-10 17:59:42 -0500

    fix meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
-end
-
-DOMAIN
-def default_url_options
-{ host: ENV[“DOMAIN”] || “localhost:3000” }
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
